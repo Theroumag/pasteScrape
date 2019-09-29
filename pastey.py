3f9dbe0e3d0990eadc49aaa1377f53785c89270a
Theroumag <40362456+Theroumag@users.noreply.github.com>
2019-09-29 16:46:14 -0700

    Add files via upload

--- pastey.py
+++ pastey.py
@@ -1,26 +1,29 @@
 import discord
 from discord.ext import commands
+import random
 
 client = commands.Bot(command_prefix = "?")
 with open("token.txt", "r") as f: token = f.read()[:-1]
 
-
-
 @client.event
 async def on_ready():
-	print("Pastey Online")
+    print("Pastey Online")
+
 
 @client.event
-async def on_member_join(member):
-	print(f"{member} has joined the server")
-
-@client.event
-async def on_member_remove(member):
-	print(f"{member} has left the server")
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == "Hi Pastey!":
+        await client.send_message(message.channel, f"Hi {message.author}!")
 
 @client.command()
 async def ping(ctx):
-	await ctx.send("pong")
+    await ctx.send(f"Pong! Latency is {round(client.latency *100)}ms ")
+
+@client.command(aliases=["purge"])
+async def clear(ctx, amount=10):
+    await ctx.channel.purge(limit=amount)
 
 
 client.run(token)
