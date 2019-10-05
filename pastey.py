52bd17f4048f658f4b1b1fc742cc8ef638575601
Theroumag <40362456+Theroumag@users.noreply.github.com>
2019-10-05 11:30:55 -0700

    Add files via upload

--- pastey.py
+++ pastey.py
@@ -1,6 +1,8 @@
-import discord, requests, time, re
+import discord, re
 from discord.ext import commands
 from bs4 import BeautifulSoup
+from torrequest import TorRequest
+from os import system
 
 client = commands.Bot(command_prefix = "?")
 with open("token.txt", "r") as f: token = f.read()[:-1]
@@ -8,10 +10,12 @@ with open("token.txt", "r") as f: token = f.read()[:-1]
 
 @client.event
 async def on_ready():
-    print("Pastey Online")
     await client.change_presence(activity=discord.Activity(name="Protecting the campus enviorment"))
     client.help_command = commands.DefaultHelpCommand(no_category='Commands')
-
+    tr=TorRequest(password=input("Unhashed Tor password: "))
+    r = tr.get("https://pastebin.com/archive")
+    system("clear")
+    print("Pastey Online")
 
 @client.command()
 async def ping(ctx):
@@ -26,30 +30,31 @@ async def start(ctx):
     await ctx.send("I have began scraping http://pastebin.com! :slight_smile:")
     linkRegrex = re.compile(r'href="/\w{8}"')
     scraped_links = []
+    keywords = []
     prune_limit = 49
+
     while True:
-        r = requests.get("https://pastebin.com/archive")
+        try:
+            r = tr.get("https://pastebin.com/archive")
+        except:
+            tr.reset_identity()
+            r = tr.get("https://pastebin.com/archive")
+
         soup = BeautifulSoup(r.text, features="lxml")
         match = soup.html.body.div.find("div", id="super_frame").div.div.find("div", id="content_left").find("div", style="padding: 0 0 10px 0").table
         links = linkRegrex.findall(str(match))
+
         for link in links:
             l = "http://pastebin.com/raw/"+link.split('/')[1][:-1]
-            if l not in scraped_links:
-                try:
-                    await ctx.send(f"Paste: {l}\n```{requests.get(l).text}```")
-                except:
-                    await ctx.send("Paste larger than 2000 characters (will fix in future)")
+            for keyword in keywords:
+                if keyword in tr.get(l).text:
+                    if l not in scraped_links:
+                        try:
+                            await ctx.send(f"Paste: {l}\n```{tr.get(l).text}```")
+                        except:
+                            await ctx.send("Paste larger than 2000 characters (will fix in future)")
             scraped_links.append(l)
         while len(scraped_links) >= prune_limit:
-            scraped_links = scraped_links[1:]
-        time.sleep(10)
-
-@client.command()
-async def bruh(ctx, amount=5):
-    if str(ctx.author) == "Theroumag#3356":
-        for x in range(amount):
-            await ctx.send(f"Goodnight man")
-    print(ctx.author)
-
+            scraped_links.pop(0)
 
 client.run(token)
